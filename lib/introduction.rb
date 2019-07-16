468f4662e01030efce9cb2478ad14ecdae3402bb
Kevin Bagnall <bubbagump2020@gmail.com>
2019-07-16 21:16:20 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,6 +6,10 @@ def introduction_with_language(name, language)
 	puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language_optional(name)
- 
+def introduction_with_language_optional(name, language = nil)
+  if language == nil
+    puts "Hi, my name is #{name} and I am learning to program in Ruby."
+  else
+    puts "Hi, my name is #{name} and I am learning to program in #{lanugage}.}"
+  end
 end
